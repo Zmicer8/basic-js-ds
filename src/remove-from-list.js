8e1ba45c60b46fc230ead94c4f450b553a2a5430
Zmicer8 <sheff8363@mail.ru>
2022-05-02 22:33:44 +0200

    init: start task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,10 +23,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+ throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+ /* class ListNode {
+       constructor(x) {
+        this.value = x;
+        this.next = null;
+       }
+      }*/
+  let list=l;
+  let index = 0;
+  while(current){
+    if (current.value==k){
+      prev.next=current.next;
+      this.lenght--;
+     // return index;
+    }
+    current=current.next;
+    index++;
+  }
+ /*   print() {
+    let current= this.head; 
+    while(current){
+      console.log(current.value);
+        current=current.next;
+      }
+  }*/
+console.log(list)
 
+}
+//removeKFromList( [3, 1, 2, 3, 4, 5], 3 )
 module.exports = {
   removeKFromList
 };
--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  arr = [];
+  push(element) {
+    this.arr.push(element);
+    //  throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.arr.pop();
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.arr[this.data.length - 1];
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
